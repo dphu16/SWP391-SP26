916540565518c4a94629291c3ccedb82346f34f7
minhduc-2012 <ducm9889@gmail.com>
2026-02-05 23:24:26 +0700

    Add function assign Kpi

--- src/main/java/com/project/hrm/evaluation/controller/KpiAcknowledgementController.java
+++ src/main/java/com/project/hrm/evaluation/controller/KpiAcknowledgementController.java
@@ -1,4 +1,23 @@
 package com.project.hrm.evaluation.controller;
 
+import com.project.hrm.evaluation.entity.KpiAcknowledgement;
+import com.project.hrm.evaluation.service.KpiAcknowledgementService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+import java.util.UUID;
+
 public class KpiAcknowledgementController {
+    private final KpiAcknowledgementService service;
+
+    public KpiAcknowledgementController(KpiAcknowledgementService service) {
+        this.service = service;
+    }
+
+
+    @PutMapping
+    public KpiAcknowledgement updateAckownledgement(@PathVariable UUID goalId, @PathVariable UUID employeeId){
+        return service.confirm(goalId, employeeId);
+    }
 }
