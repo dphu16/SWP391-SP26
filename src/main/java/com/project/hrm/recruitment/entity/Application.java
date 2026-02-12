b797298abb7b51df33da6c92f15208de8e97938d
MinhQuanK19 <quanhoctoanlihoa@gmail.com>
2026-02-12 19:53:09 +0700

    Update Application Entity

--- src/main/java/com/project/hrm/recruitment/entity/Application.java
+++ src/main/java/com/project/hrm/recruitment/entity/Application.java
@@ -1,13 +1,12 @@
 package com.project.hrm.recruitment.entity;
 
-import com.project.hrm.recruitment.enums.ApplicationStatus;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 
-import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -16,30 +15,34 @@ import java.util.UUID;
 @Entity
 @Table(name = "applications")
 public class Application {
-
     @Id
     @ColumnDefault("uuid_generate_v4()")
     @Column(name = "app_id", nullable = false)
     private UUID id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "req_id")
-    private JobRequisition req;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "candidate_id")
-    private Candidate candidate;
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "job_id", nullable = false)
+    private Job job;
 
     @NotNull
-    @Enumerated(EnumType.STRING)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "candidate_id", nullable = false)
+    private Candidate candidate;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "cv_url", nullable = false)
+    private String cvUrl;
+
+    @Size(max = 30)
     @ColumnDefault("'APPLIED'")
     @Column(name = "status", length = 30)
-    private ApplicationStatus status;
-
-    @Column(name = "ai_match_score", precision = 5, scale = 2)
-    private BigDecimal aiMatchScore;
+    private String status;
 
     @ColumnDefault("now()")
-    @Column(name = "applied_at")
-    private OffsetDateTime appliedAt;
+    @Column(name = "created_at")
+    private OffsetDateTime createdAt;
+
+
 }
