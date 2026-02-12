6f20b93dfbf378330b82415f8cc8915aadf32deb
MinhQuanK19 <quanhoctoanlihoa@gmail.com>
2026-02-12 19:56:21 +0700

    Update Entity

--- src/main/java/com/project/hrm/recruitment/entity/Interview.java
+++ src/main/java/com/project/hrm/recruitment/entity/Interview.java
@@ -1,13 +1,13 @@
 package com.project.hrm.recruitment.entity;
 
-import com.project.hrm.recruitment.enums.InterviewStatus;
+import com.project.hrm.module.corehr.entity.Employee;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 
-import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -16,15 +16,15 @@ import java.util.UUID;
 @Entity
 @Table(name = "interviews")
 public class Interview {
-
     @Id
     @ColumnDefault("uuid_generate_v4()")
     @Column(name = "interview_id", nullable = false)
     private UUID id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "app_id")
-    private Application application;
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "app_id", nullable = false)
+    private Application app;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "interviewer_id")
@@ -33,15 +33,13 @@ public class Interview {
     @Column(name = "schedule_time")
     private OffsetDateTime scheduleTime;
 
-    @NotNull
-    @Enumerated(EnumType.STRING)
+    @Size(max = 20)
     @ColumnDefault("'SCHEDULED'")
     @Column(name = "status", length = 20)
-    private InterviewStatus status;
+    private String status;
 
     @Column(name = "feedback", length = Integer.MAX_VALUE)
     private String feedback;
 
-    @Column(name = "rating", precision = 4, scale = 2)
-    private BigDecimal rating;
+
 }
