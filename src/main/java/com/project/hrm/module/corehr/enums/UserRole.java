2814ce0a40b311b3ac1aefdb2fd11c4418a26bb4
ducanh <duc4nh2503@gmail.com>
2026-02-13 23:04:06 +0700

    update base-code

--- src/main/java/com/project/hrm/module/corehr/enums/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.project.hrm.module.corehr.enums;
-
-public enum UserRole {
-    MENTOR, HR, MANAGER, EMPLOYEE, FINANCE
-}
-
--- src/main/java/com/project/hrm/module/corehr/enums/UserStatus.java
+++
@@ -1,5 +0,0 @@
-package com.project.hrm.module.corehr.enums;
-
-public enum UserStatus {
-    ACTIVE, INACTIVE, LOCKED, PENDING_VERIFICATION
-}
