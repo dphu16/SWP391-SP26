dc0b6459ac833af80c86a65c32aef687d6c1e714
minhduc-2012 <ducm9889@gmail.com>
2026-02-26 13:55:26 +0700

    Test 3

--- backend/src/main/java/com/project/hrm/module/payroll/common/enums/PayslipStatus.java
+++
@@ -1,8 +0,0 @@
-package com.project.hrm.module.payroll.common.enums;
-
-public enum PayslipStatus {
-    DRAFT,
-    CONFIRMED,
-    PAID,
-    CANCELLED
-}
--- backend/src/main/java/com/project/hrm/module/payroll/common/enums/PayslipType.java
+++
@@ -1,6 +0,0 @@
-package com.project.hrm.module.payroll.common.enums;
-
-public enum PayslipType {
-    INCOME,
-    DEDUCTION
-}
